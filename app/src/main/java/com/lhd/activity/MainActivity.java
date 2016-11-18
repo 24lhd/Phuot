c385f33ffdfbae48e2eb0e2b040a516efeaf050b
LeHongDuong <24duong@gmail.com>
2016-11-18 14:26:35 +0700

    sử dụng navigation và gg map

---
+++ app/src/main/java/com/lhd/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.lhd.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lhd.phuot.R;
+
+
+/**
+ * Created by Duong on 11/15/2016.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
---
+++ app/src/main/java/com/lhd/fragment/ABC.java
@@ -0,0 +1,9 @@
+package com.lhd.fragment;
+
+/**
+ * Created by Duong on 11/15/2016.
+ */
+
+public class ABC {
+
+}
